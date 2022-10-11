8665131de542be37c048947106a180e124a82783
nickjeon <nicholaskjeon@gmail.com>
2022-10-10 21:37:39 -0400

    added ease animation

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled'
 import {
 	animate,
+	AnimationOptions,
 	motion,
 	useMotionValue,
-	useTransform
+	useTransform,
+	useMotionTemplate,
+	useVelocity
 } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import Cell, { CELL_SIZE } from './Cell'
@@ -19,6 +22,13 @@ const Container = styled(motion.div)<{
 	overflow: hidden;
 	display: grid;
 	grid-template-columns: repeat(${(props) => props.columns}, 1fr);
+	mask-image: radial-gradient(
+		300px 300px,
+		rgba(0, 0, 0, 1),
+		rgba(0, 0, 0, 0.4),
+		transparent
+	);
+	mask-repeat: no-repeat;
 `
 
 function Grid() {
@@ -29,12 +39,41 @@ function Grid() {
 	const mouseX = useMotionValue(0)
 	const mouseY = useMotionValue(0)
 
+	const centerMouseX = useTransform<number, number>(mouseX, (newX) => {
+		return newX - window.innerWidth / 2
+	})
+	const centerMouseY = useTransform<number, number>(mouseY, (newY) => {
+		return newY - window.innerHeight / 2
+	})
+	const WebkitMaskPosition = useMotionTemplate`${centerMouseX}px ${centerMouseY}px`
+
+	// eased mouse position
+	const mouseXEased = useMotionValue(0)
+	const mouseYEased = useMotionValue(0)
+
+	// mouse velocity
+	const mouseXVelocity = useVelocity(mouseXEased)
+	const mouseYVelocity = useVelocity(mouseYEased)
+	const mouseVelocity = useTransform<number, number>(
+		[mouseXVelocity, mouseYVelocity],
+		([latestX, latestY]) => Math.abs(latestX) + Math.abs(latestY)
+	)
+	// map mouse velocity to an opacity value
+	const opacity = useTransform(mouseVelocity, [0, 1000], [0, 1])
+
 	// handle mouse move on document
 	useEffect(() => {
 		const handleMouseMove = (e: MouseEvent) => {
 			// animate mouse x and y
 			animate(mouseX, e.clientX)
 			animate(mouseY, e.clientY)
+			// animate eased mouse x and y
+			const transition: AnimationOptions<number> = {
+				ease: 'easeOut',
+				duration: 1,
+			}
+			animate(mouseXEased, e.clientX, transition)
+			animate(mouseYEased, e.clientY, transition)
 		}
 		// recalculate grid on resize
 		window.addEventListener('mousemove', handleMouseMove)
@@ -64,7 +103,7 @@ function Grid() {
 	}, [])
 
 	return (
-		<Container columns={columns}>
+		<Container columns={columns} style={{ WebkitMaskPosition }}>
 			{Array.from({ length: columns * rows }).map((_, i) => (
 				<Cell key={i} mouseX={mouseX} mouseY={mouseY} />
 			))}
